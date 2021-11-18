c14ba83c35cd50ebdb0e86e11975af5d1eda8a3f
Hes097 <henning097@live.com>
2021-11-18 17:02:46 +0100

    changes

--- ex-sync.R
+++ ex-sync.R
@@ -2,20 +2,22 @@
 # https://stackoverflow.com/questions/55562652/calling-r-script-from-node-js-issue-loading-libraries
 
 #Loading packages
-#needs(sp)
-#needs(sf)
-#needs(raster)
-#needs(caret)
-#needs(mapview)
-#needs(CAST)
-#needs(tmap)
-#needs(latticeExtra)
-#needs(doParallel)
-#needs(parallel)
-#needs(Orcs)
+needs(sp)
+needs(sf)
+needs(raster)
+needs(caret)
+needs(mapview)
+needs(CAST)
+needs(tmap)
+needs(latticeExtra)
+needs(doParallel)
+needs(parallel)
+needs(Orcs)
 
+#Das Laden der Datei hat nur funktioniert, als alle Dateien der Beispieldaten vorhanden waren.
+sen_ms <- stack("./Files/Sen_Muenster.grd")
 
-num = strtoi(input[[1]]) * 2
+num = strtoi(input[[1]]) * 3
 
 x = 25 
 
