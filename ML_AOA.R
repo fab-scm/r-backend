9cca7c0084ff7370b813c7a89baadd7d091d6f91
fbruc03 <65135023+fbruc03@users.noreply.github.com>
2021-11-23 16:10:21 +0100

    test r-integration package

--- ML_AOA.R
+++ ML_AOA.R
@@ -1,6 +1,10 @@
+x <- function(data) {
+
+data = 2 * data
+
 rm(list=ls())
 #major required packages:
-needs(raster, lib.loc="./r-library")
+needs(raster)
 needs(caret)
 needs(mapview)
 needs(sf)
@@ -14,25 +18,20 @@ needs(doParallel)
 needs(parallel)
 needs(Orcs)
 
-sen_ms <- stack("Files/Sen_Muenster.grd")
+sen_ms <- stack("data/Sen_Muenster.grd")
 #print(sen_ms)
 
-# rgbplot_ms <- spplot(sen_ms[[1]],  col.regions="transparent",sp.layout =rgb2spLayout(sen_ms[[1:3]], quantiles = c(0.02, 0.98), alpha = 1))
+#plot(sen_ms)
 
-sen_mr <- stack("Files/Sen_Marburg.grd")
-# rgbplot_mr <- spplot(sen_mr[[1]],  col.regions="transparent",sp.layout =rgb2spLayout(sen_mr[[1:3]], quantiles = c(0.02, 0.98), alpha = 1))
-
-# plot(sen_ms)
-
-trainSites <- read_sf("Files/trainingsites_muenster.gpkg")
+trainSites <- read_sf("data/trainingsites_muenster.gpkg")
 #print(trainSites)
 
-#viewRGB(sen_ms, r = 3, g = 2, b = 1, map.types = "Esri.WorldImagery")
-  # mapview(trainSites)
+viewRGB(sen_ms, r = 3, g = 2, b = 1, map.types = "Esri.WorldImagery")+
+  mapview(trainSites)
 
-extr_pixel <- extract(sen_ms, trainSites, df=TRUE)
-extr <- merge(extr_pixel, trainSites, by.x="ID", by.y="PolygonID")
-# head(extr)
+extr <- extract(sen_ms, trainSites, df=TRUE)
+extr <- merge(extr, trainSites, by.x="ID", by.y="PolygonID")
+head(extr)
 
 set.seed(100)
 trainids <- createDataPartition(extr$ID,list=FALSE,p=0.05)
@@ -55,3 +54,7 @@ model <- ffs(trainDat[,predictors],
                trControl=ctrl,
                importance=TRUE,
                ntree=75)
+
+#print(model)
+return(model)
+}
--- app.js
+++ app.js
@@ -1,36 +1,27 @@
 var express = require('express')
-var app = express(); 
+//var R = require("r-script");
+const R = require('r-integration');
+var app = express();
 
-// app.get('/', function(req, res) {
-//     res.send("Test")
-// })
-
-var server = app.listen(9090, function() {
+var server = app.listen(9090, function () {
     var port = server.address().port
     console.log(`App listening at http://localhost:${port}`)
 })
 
-// Versuch das r-script über eine Route, also auf dem Server laufen zu lassen - funktioniert noch nicht
-// glaube das Problem liegt an der Ausführung von needs - versucht die Packages über meine vorhandenen Packages zu installieren
-// wie schaffe ich es eine library in meinem Repository anzulegen, auf welche zurückgegriffen wird?
-var R = require("r-script");
-
-app.get('/', (req, res, next) => {
-    console.log("Test")
-
-    // sync
-    // var out = R("ex-sync.R")
-    //     .data(20)
-    //     .callSync();
-
-    // async
-    var out = R("ex-sync.R")
-        .data(20)
-        .call(function(err, d) {
-            if (err) throw err;
-            return d;
-        });
-  
-
+app.get('/sync', (req, res, next) => {
+    console.log("sync test")
+    var out = R("ML_AOA.R")
+        .data()
+        .callSync();
     console.log(out);
+})
+
+app.get('/async', (req, res, next) => {
+    callMethodAsync("ML_AOA.R", "x", ["2"]).then((result) => {
+        console.log(result);
+        res.send(result);
+    }).catch((error) => {
+        console.error(error);
+        res.send(error);
+    })
 })
