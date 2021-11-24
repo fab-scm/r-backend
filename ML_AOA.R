16b6e6d50fb0a2477c58fc6000bf18c5b75382e5
Hes097 <henning097@live.com>
2021-11-24 13:05:47 +0100

    test

--- ML_AOA.R
+++ ML_AOA.R
@@ -2,59 +2,59 @@ x <- function(data) {
 
 data = 2 * data
 
-rm(list=ls())
+#rm(list=ls())
 #major required packages:
-needs(raster)
-needs(caret)
-needs(mapview)
-needs(sf)
+#needs(raster)
+#needs(caret)
+#needs(mapview)
+#needs(sf)
 #needs(devtools)
 #install_github("HannaMeyer/CAST")
-needs(CAST)
+#needs(CAST)
 #additional required packages:
-needs(tmap)
-needs(latticeExtra)
-needs(doParallel)
-needs(parallel)
-needs(Orcs)
+#needs(tmap)
+#needs(latticeExtra)
+#needs(doParallel)
+#needs(parallel)
+#needs(Orcs)
 
-sen_ms <- stack("data/Sen_Muenster.grd")
+#sen_ms <- stack("data/Sen_Muenster.grd")
 #print(sen_ms)
 
 #plot(sen_ms)
 
-trainSites <- read_sf("data/trainingsites_muenster.gpkg")
+#trainSites <- read_sf("data/trainingsites_muenster.gpkg")
 #print(trainSites)
 
-viewRGB(sen_ms, r = 3, g = 2, b = 1, map.types = "Esri.WorldImagery")+
-  mapview(trainSites)
+#viewRGB(sen_ms, r = 3, g = 2, b = 1, map.types = "Esri.WorldImagery")+
+#  mapview(trainSites)
 
-extr <- extract(sen_ms, trainSites, df=TRUE)
-extr <- merge(extr, trainSites, by.x="ID", by.y="PolygonID")
-head(extr)
+#extr <- extract(sen_ms, trainSites, df=TRUE)
+#extr <- merge(extr, trainSites, by.x="ID", by.y="PolygonID")
+#head(extr)
 
-set.seed(100)
-trainids <- createDataPartition(extr$ID,list=FALSE,p=0.05)
-trainDat <- extr[trainids,]
+#set.seed(100)
+#trainids <- createDataPartition(extr$ID,list=FALSE,p=0.05)
+#trainDat <- extr[trainids,]
 
-predictors <- names(sen_ms)
-response <- "Label"
+#predictors <- names(sen_ms)
+#response <- "Label"
 
-indices <- CreateSpacetimeFolds(trainDat,spacevar = "ID",k=3,class="Label")
-ctrl <- trainControl(method="cv", 
-                     index = indices$index,
-                     savePredictions = TRUE)
+#indices <- CreateSpacetimeFolds(trainDat,spacevar = "ID",k=3,class="Label")
+#ctrl <- trainControl(method="cv", 
+#                     index = indices$index,
+#                     savePredictions = TRUE)
 
 # train the model
-set.seed(100)
-model <- ffs(trainDat[,predictors],
-               trainDat[,response],
-               method="rf",
-               metric="Kappa",
-               trControl=ctrl,
-               importance=TRUE,
-               ntree=75)
+#set.seed(100)
+#model <- ffs(trainDat[,predictors],
+#               trainDat[,response],
+#               method="rf",
+#               metric="Kappa",
+#              trControl=ctrl,
+#               importance=TRUE,
+#               ntree=75)
 
 #print(model)
-return(model)
+return(data)
 }
