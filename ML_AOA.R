919f0918299f2b19e5eca713e7cb89a54afcc0b5
Hes097 <henning097@live.com>
2021-11-29 17:01:32 +0100

    aenderungen waehrend unseres meetings (29.11)

--- ML_AOA.R
+++ ML_AOA.R
@@ -132,23 +132,23 @@ train <- function() {
 classifyAndAOA <- function(data) {
 
     library(needs)
-    needs(sp)
-    needs(raster) 
-    # library(CAST) 
-    # library(tmap)
-    # library(latticeExtra)
-    # library(doParallel)
-    # library(parallel)
-    # library(Orcs)
+    library(sp)
+    library(raster) 
+    library(CAST) 
+    library(tmap)
+    library(latticeExtra)
+    library(doParallel)
+    library(parallel)
+    library(Orcs)
 
     sen_ms <- stack("data/Sen_Muenster.grd")
-    #model <- readRDS("./tempModel/model.RDS")
+    model <- readRDS("tempModel/model.RDS")
 
-    #prediction <- predict(sen_ms,model)
-    #writeRaster(prediction, "./stack/prediction.tif")
+    prediction <- predict(sen_ms,model)
+    writeRaster(prediction, "stack/prediction.tif", overwrite=TRUE)
 
-    #cl <- makeCluster(4)
-    #registerDoParallel(cl)
-    #AOA <- aoa(sen_ms,model,cl=cl)
-    #writeRaster(AOA, "./stack/aoa.tif")
+    cl <- makeCluster(4)
+    registerDoParallel(cl)
+    AOA <- aoa(sen_ms,model,cl=cl)
+    writeRaster(AOA, "stack/aoa.tif", overwrite=TRUE)
 }
